a09530b66af512aee03d43b07748977260762837
rubileandro <rubi@brislandventures.com>
2024-01-04 10:57:11 +0400

    Hello, World!

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+Author: Rubi
+Program: Hello, World!
+Description: This program prints hello, world to the screen!
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    // print 3 lines
+    printf("Hello, World\n");
+    printf("Line 2!\n"); // line 2
+    printf("Line3!\n");
+
+    return 0;
+}
