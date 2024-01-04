8f88033e8c45655c93d25e4de355ea71a179695b
rubileandro <rubi@brislandventures.com>
2024-01-04 12:14:01 +0400

    modulus

--- main.c
+++ main.c
@@ -2,17 +2,12 @@
 
 int main(void)
 {
-    double x = 5.2, y = 2.5;
+    //modulus
+    int x = 11, y = 3;
 
-    double mult = x * y;
-    double add = x + y;
-    double div = x / y;
-    double sub = x - y;
+    int div = x / y;
+    int mod = x % y;
 
-    printf("mult: %f\n", mult);
-    printf("add: %f\n", add);
-    printf("div: %f\n", div);
-    printf("sub: %f\n", sub);
-
-    return 0;
+    printf("div: %d\n", div);
+    printf("div: %d\n", mod);
 }
