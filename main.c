b626360bfb9e4a86884312fb5eeb80755e5aff25
rubileandro <rubi@brislandventures.com>
2024-01-04 12:06:35 +0400

    Basic arithmetic operators

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 
 int main(void)
 {
-    char c = 'd';
-    int speed = 10;
-    double x = 5.5;
-    float y = -54.1;
+    double x = 5.2, y = 2.5;
 
-    printf("c: ");
-    scanf("%c", &c);
-    printf("c: %c\n", c);
-    printf("c: %d\n", c);
+    double mult = x * y;
+    double add = x + y;
+    double div = x / y;
+    double sub = x - y;
+
+    printf("mult: %f\n", mult);
+    printf("add: %f\n", add);
+    printf("div: %f\n", div);
+    printf("sub: %f\n", sub);
 
     return 0;
 }
