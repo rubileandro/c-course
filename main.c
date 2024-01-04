d9d65b12dfc54575839fdcaa8dd9242105cb59d8
rubileandro <rubi@brislandventures.com>
2024-01-04 13:18:47 +0400

    Control flow if / else if / else

--- main.c
+++ main.c
@@ -3,22 +3,17 @@
 int main(void)
 {
     int grade = 0;
-
     printf("Grade ");
     scanf("%d", &grade);
 
-    if (grade >= 50)
+   if (grade >= 90) printf("A\n");
+   else if (grade >= 80) printf("B\n");
+   else if (grade >= 70) printf("C\n");
+   else if (grade >= 60)
     {
-        printf("Pass\n");
-        printf("Congrats!\n");
+        printf("D\n");
     }
-    else
-    {
-        printf("Fail\n");
-        printf("Good luck next time!\n");
-    }
-
-    printf("If done\n");
+   else printf("Fail\n");
 
     return 0;
 }
