1ac701500cec5276ac817ef2838978b8a63e6843
rubileandro <rubi@brislandventures.com>
2024-01-08 20:53:28 +0400

    Switch statement with break

--- main.c
+++ main.c
@@ -2,38 +2,22 @@
 
 int main(void)
 {
+    int number = 0;
 
-    double initial = 0, step = 0, stop = 0;
-
-    do
+    switch (number)
     {
-        printf("Initial (m): ");
-        scanf("%lf", &initial);
-        if (initial <0) printf("Must be >=0\n");
-    }   while (initial < 0);
+        case 0:
+            printf("Case 0!\n");
+            break;
 
-    do
-    {
-        printf("Step (m): ");
-        scanf("%lf", &step);
-        if (step <=0) printf("Must be >0\n");
-    }   while (step < 0);
+        case 1:
+            printf("Case 1!\n");
 
-    do
-    {
-        printf("Stop (m): ");
-        scanf("%lf", &stop);
-        if (stop <0) printf("Must be >=0\n");
-    }   while (stop < 0);
-
-    printf("\n");
-    printf("Meters    Feet\n");
-    printf("********************\n");
-    for (double  conv = initial;
-        conv <= stop;
-        conv += step)
-    {
-        printf("%.2f %.2f\n", conv, conv * 3.28084);
+        case 2:
+        {
+            printf("Case 2!\n");
+        }
+            printf("Switch over!\n");
     }
 
 
