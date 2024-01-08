397507c04c38fca8d57791723893d943de24767c
rubileandro <rubi@brislandventures.com>
2024-01-08 20:59:52 +0400

    Switch statement with default

--- main.c
+++ main.c
@@ -2,23 +2,21 @@
 
 int main(void)
 {
-    int number = 0;
+    int number = 3;
 
     switch (number)
     {
         case 0:
             printf("Case 0!\n");
-            break;
 
         case 1:
             printf("Case 1!\n");
 
-        case 2:
-        {
-            printf("Case 2!\n");
-        }
-            printf("Switch over!\n");
+        default:
+            printf("Default case!\n");
     }
+
+    printf("Switch over!\n");
 
 
     return 0;
