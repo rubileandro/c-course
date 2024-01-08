773587cd16582642f87d3d711a9d5a07caf24ea7
rubileandro <rubi@brislandventures.com>
2024-01-08 19:30:50 +0400

    Meters to Feet Conversion with Do-While Loops. Also using the format specifier with printf.

--- main.c
+++ main.c
@@ -3,10 +3,39 @@
 int main(void)
 {
 
-    for (int i = 0; i < 10; i++)
+    double initial = 0, step = 0, stop = 0;
+
+    do
     {
-        printf("i: %d\n", i);
+        printf("Initial (m): ");
+        scanf("%lf", &initial);
+        if (initial <0) printf("Must be >=0\n");
+    }   while (initial < 0);
+
+    do
+    {
+        printf("Step (m): ");
+        scanf("%lf", &step);
+        if (step <=0) printf("Must be >0\n");
+    }   while (step < 0);
+
+    do
+    {
+        printf("Stop (m): ");
+        scanf("%lf", &stop);
+        if (stop <0) printf("Must be >=0\n");
+    }   while (stop < 0);
+
+    printf("\n");
+    printf("Meters    Feet\n");
+    printf("********************\n");
+    for (double  conv = initial;
+        conv <= stop;
+        conv += step)
+    {
+        printf("%.2f %.2f\n", conv, conv * 3.28084);
     }
+
 
     return 0;
 }
