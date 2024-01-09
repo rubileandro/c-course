583a1a7607f309c0017cd94623673b96fbf9a88b
rubileandro <rubi@brislandventures.com>
2024-01-09 09:09:45 +0400

    trying to print a null terminator

--- strings.c
+++ strings.c
@@ -3,17 +3,16 @@
 int main(void)
 {
 
-    char mydata[5];
-    mydata[0] = 'a';
-    mydata[1] = 'b';
-    mydata[2] = 'c';
-    mydata[3] = 'd';
-    mydata[4] = '\0';
+    char s1[] = "This is my string.";
 
-    printf("mydata[2] = %c\n", mydata[2]);
-
-    printf("\n%s\n", mydata);
-
+    for (int i = 0; i < 19; i++)
+    {
+        if (s1[i] == '\0')
+        printf("s1[%d] = \\0 null terminator\n", i);
+        else
+        printf("s1[%d] = %c\n", i, s1[i]);
+    }
+    printf("\n%s\n", s1);
 
     return 0;
 }
