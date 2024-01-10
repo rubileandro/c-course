fc3e768b4ea5ae579671574430aaeb193eec305d
rubileandro <rubi@brislandventures.com>
2024-01-10 07:59:22 +0400

    Using strcmp to compare two strings for equality at each index.

--- strings.c
+++ strings.c
@@ -3,21 +3,13 @@
 
 int main(void)
 {
+    char s1[] = "This is a string.";
+    char s2[] = "This is A string.";
 
-    char s1[] = "This is the way.";
-
-    int length = strlen(s1);
-    int num_is = 0;
-
-    for (int i = 0; i < length; i++)
-        if (s1[i] == 'i') num_is++;
-
-    printf("Length: %d\n", length);
-    printf("Number of is: %d\n", num_is);
-
-
-
-
+    if (strcmp(s1,s2) == 0)
+        printf("s1 and s2 are equal.\n");
+    else
+        printf("s1 and s1 are not equal.\n");
 
     return 0;
 }
