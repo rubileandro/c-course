eccf102d6c1866d0a28101b3e01fbb215c671f92
rubileandro <rubi@brislandventures.com>
2024-01-11 07:45:27 +0400

    Calculating max value and adding 10.

--- func.c
+++ func.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int find_max(int a, int b)
-{
-    if (a > b) return a;
-    else return b;
-}
+int find_max(int a, int b);
 
 int main(void)
 {
-    int max = find_max(10, 5);
-    printf("Max: %d\n", max);
+    int max = find_max(10, 12) + 10;
+    printf("Result: %d\n", max);
 
     return 0;
+}
+
+int find_max(int a, int b)
+{
+if (a > b) return a;
+else return b;
 }
