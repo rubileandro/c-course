be039fe6c8c1bd8cc67f7ef8d8c1a85112a97052
rubileandro <rubi@brislandventures.com>
2024-01-12 09:47:03 +0400

    Pass by pointer

--- pointers.c
+++ pointers.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
+void add_one(int *a);
+
 int main(void)
 {
-    int a = 5;
-    int b = 10;
-    int *p;
-    p = &b;
-    printf("p: %p\n", p);
-    printf("&b: %p\n", &b );
-    printf("&a: %p\n", &a );
-    p = &a;
-    printf("p: %p\n", p);
-    *p = *p + 1;
-    printf("a: %d\n", a);
+    int b = 5;
+    printf("&b: %p\n", &b);
+    add_one(&b);
+    printf("b: %d\n", b);
 
 
     return 0;
+}
+
+void add_one(int *a)
+{
+    printf(" a: %p\n", a);
+    *a = *a + 1;
 }
