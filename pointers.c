da8444d7103dd930e9e4ba31f87c08c34768741e
rubileandro <rubi@brislandventures.com>
2024-01-12 08:45:49 +0400

    Pointers and printings addresses.

---
+++ pointers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int b = 10;
+    int *p;
+    p = &b;
+    printf("p: %p\n", p);
+    printf("&b: %p\n", &b );
+    printf("&a: %p\n", &a );
+    p = &a;
+    printf("p: %p\n", p);
+    *p = *p + 1;
+    printf("a: %d\n", a);
+
+
+    return 0;
+}
