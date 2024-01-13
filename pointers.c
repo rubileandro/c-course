213c2ee3132bfa9482ed26879a2a11bc04a78113
rubileandro <rubi@brislandventures.com>
2024-01-13 10:24:38 +0400

    Using array notation with pointers.

--- pointers.c
+++ pointers.c
@@ -6,9 +6,14 @@ int main(void)
 {
 
     int a[] = {1,2,3};
-    add_one(a, 3);
-    for (int i = 0; i < 3; i++)
-        printf("a[%d]=%d\n", i, a[i] );
+    int *p;
+    p = a;
+
+    printf("p: %p\n", p );
+    printf("a: %p\n", a );
+
+    printf("p[2] = %d\n", p[2]);
+    printf("a[2] = %d\n", a[2]);
 
     return 0;
 }
