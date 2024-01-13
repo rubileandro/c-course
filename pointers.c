730b8b2887f1ce3a2fa2cc650b6d4e595266716c
rubileandro <rubi@brislandventures.com>
2024-01-13 12:59:51 +0400

    Using malloc and array notaiton.

--- pointers.c
+++ pointers.c
@@ -4,16 +4,19 @@ void add_one(int *array, int length);
 
 int main(void)
 {
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
-    add_one(a + 5, 5);
-    for (int i = 0; i < 10; i++)
-        printf("a[%d] = %d\n", i, a[i]);
+    int *a;
+    a = malloc( sizeof(int) * 5 );
+
+    a[0] = 1;
+    a[1] = 5;
+    a[2] = 7;
+    a[3] = 9;
+    a[4] = 11;
+    for (int i = 0; i < 5; i++)
+    printf("a[%d] = %d\n", i , a[i]);
+
 
     return 0;
 }
 
-void add_one(int *array, int length)
-{
-    for (int i = 0; i < length; i++)
-        array[i] += 1;
-}
+
