e82948141c5c0e9d8c4e938ec80b47f1242da546
rubileandro <rubi@brislandventures.com>
2024-01-13 10:02:09 +0400

    Using pointers with arrays

--- pointers.c
+++ pointers.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-void add_one(int *a, int *b, int *c);
+void add_one(int *array, int length);
 
 int main(void)
 {
-    int x1 = 1, x2 = 2, x3 = 3;
-    add_one(&x1, &x2, &x3);
-    printf("x1: %d\n", x1);
-    printf("x2: %d\n", x2);
-    printf("x3: %d\n", x3);
 
+    int a[] = {1,2,3};
+    add_one(a, 3);
+    for (int i = 0; i < 3; i++)
+        printf("a[%d]=%d\n", i, a[i] );
 
     return 0;
 }
 
-void add_one(int *a, int *b, int *c)
+void add_one(int *array, int length)
 {
-    *a = *a + 1;
-    *b = *b + 1;
-    *c = *c + 1;
+    for (int i = 0; i < length; i++)
+        array[i] += 1;
 }
