c974763a4d1c6ba8e32643a140144c7308498e26
rubileandro <rubi@brislandventures.com>
2024-01-15 09:54:45 +0400

    Finding the average of nubmers that a user inputs, using dynamically allocated memory.

--- pointers.c
+++ pointers.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
-void add_one(int *array, int length);
-
 int main(void)
+
 {
-    int *a;
-    a = malloc( sizeof(int) * 5 );
+    int *numbers;
+    int size = 2;
+    numbers = malloc (sizeof(int) * size);
+    int input = 0, i = 0;
+    do
+    {
+        if (i == size)
+        {
+            size += 2;
+            numbers = realloc(numbers, sizeof(int)*size);
+            printf("Realloc for %d ints\n", size);
+        }
+        printf("Enter (-1 to quit): ");
+        scanf("%d", &input);
+        if (input != -1)
+        {
+            numbers[i] = input;
+            i++;
+        }
 
-    a[0] = 1;
-    a[1] = 5;
-    a[2] = 7;
-    a[3] = 9;
-    a[4] = 11;
-    for (int i = 0; i < 5; i++)
-    printf("a[%d] = %d\n", i , a[i]);
+    } while (input != -1);
 
+    int num_elemnts = i;
+    int total = 0;
+    for (int j = 0; j < num_elemnts; j++)
+        total += numbers[j];
+    printf("Average: %d\n", total / num_elemnts);
+
+    free(numbers);
 
     return 0;
 }
