a25c7f0f67b080a421d298e175f9cba3682722df
rubileandro <rubi@brislandventures.com>
2024-01-16 10:43:50 +0400

    Division with type casting.

--- type.c
+++ type.c
@@ -1,36 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct
+int main(int argc, char *argv[])
 {
-    int data1;
-    int *array;
-} Info;
+    int a = 5;
+    int b = 2;
 
+    double c = (double) a / b;
 
-int main(void)
-{
-    Info a;
-    a.data1 = 7;
-    a.array = malloc(sizeof(int) * 5);
-    for (int i = 0; i < 5; i++)
-        a.array[i] = i + 1;
-
-    Info b = a;
-    printf("b.data1: %d\n", b.data1);
-    for (int i = 0; i < 5; i++)
-        printf("b.array[%d = %d\n", i , b.array[i]);
-
-    printf("a.array: %p\n", a.array);
-    printf("b.array: %p\n", b.array);
-
-    b.array[0] = 10;
-    for (int i = 0; i < 5; i++)
-        printf("b.array[%d = %d\n", i , b.array[i]);
-
-
-    free(a.array);
+    printf("%d / %d = %d\n", a, b, a / b);
+    printf("%d / %d = %f\n", a, b, c);
 
     return 0;
 }
