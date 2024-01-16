8e8e0f30ebce4f091a0309d89b895f7b103edc2a
rubileandro <rubi@brislandventures.com>
2024-01-16 08:22:22 +0400

    2D array using constants.

---
+++ 2d.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 2
+#define COLS 3
+
+
+int main(void)
+{
+    int data[ROWS][COLS] = { {1,2,3},
+                             {4,5,6}};
+
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+        {
+        printf("data [%d][%d] = ", i, j);
+        scanf("%d", &data[i][j]);
+        }
+
+
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            printf("data[%d][%d] = %d\n", i, j,
+                                             data[i][j]);
+
+
+    return 0;
+}
