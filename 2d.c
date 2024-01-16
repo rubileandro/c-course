ab3693bbcba2c35f108870984e8d584e8fffad5d
rubileandro <rubi@brislandventures.com>
2024-01-16 08:30:57 +0400

    Passing 2D array to function.

--- 2d.c
+++ 2d.c
@@ -4,11 +4,11 @@
 #define ROWS 2
 #define COLS 3
 
+void print_array(int data[ROWS][COLS]);
 
 int main(void)
 {
-    int data[ROWS][COLS] = { {1,2,3},
-                             {4,5,6}};
+    int data[ROWS][COLS] = { 0 };
 
     for (int i = 0; i < ROWS; i++)
         for (int j = 0; j < COLS; j++)
@@ -17,12 +17,16 @@ int main(void)
         scanf("%d", &data[i][j]);
         }
 
-
-    for (int i = 0; i < ROWS; i++)
-        for (int j = 0; j < COLS; j++)
-            printf("data[%d][%d] = %d\n", i, j,
-                                             data[i][j]);
+    print_array(data);
 
 
     return 0;
+}
+
+void print_array(int data[ROWS][COLS])
+{
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            printf("data[%d][%d] = %d\n", i, j,
+                                            data[i][j]);
 }
